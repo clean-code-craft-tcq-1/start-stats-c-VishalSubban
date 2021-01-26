3b3268b47b8f10a959c830633edd730cac32d4ca
VishalSubban <VishalSubban@users.noreply.github.com>
2021-01-26 22:39:20 +0530

    computing min, max & avg

--- stats.c
+++ stats.c
@@ -1,10 +1,33 @@
 #include "stats.h"
 
-struct Stats compute_statistics(const float* numberset, int setlength) {
+int i, key, j, sum = 0;
+
+struct Stats compute_statistics(const float* numberset, int setlength)
+{
     struct Stats s;
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;
+    
+    /* Sorting range of numbers */
+    for (i = 1; i < n; i++) 
+    { 
+        key = numberset[i]; 
+        j = i - 1; 
+        while (j >= 0 && numberset[j] > key) { 
+            numberset[j + 1] = numberset[j]; 
+            j = j - 1; 
+        } 
+        numberset[j + 1] = key; 
+    }
+    
+    s.min = numberset[0];
+    s.max = numberset[setlength-1]; 
+        
+    /* Average of numbers */
+    for(i=0;i<setlength;i++)
+    {
+        sum += numberset[i];
+    }
+    s.average = sum/setlength;
+
 }
 
 int emailAlertCallCount = 0;
