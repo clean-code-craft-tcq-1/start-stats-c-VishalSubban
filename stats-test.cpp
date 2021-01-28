06027d2b2e754742f7e89eb990c1f8eda34d7c93
VishalSubban <VishalSubban@users.noreply.github.com>
2021-01-28 19:27:20 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -20,7 +20,7 @@ TEST_CASE("reports average, minimum and maximum") {
 
 TEST_CASE("average is NaN for empty array") {
     int setlength = 0;
-    float numberset[4] = {1.5/setlength, 8.9/setlength, 3.2/setlength, 4.5/setlength};
+    float numberset[];
     stats_s computedStats;
     computedStats = compute_statistics(numberset, setlength);
     
