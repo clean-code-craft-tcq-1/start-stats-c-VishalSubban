b1804e8f297482a9d4d0ce24adaa88db6435ecc4
VishalSubban <VishalSubban@users.noreply.github.com>
2021-01-28 21:10:45 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -1,16 +1,20 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+/******************************************************* MACROS *******************************************************/
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main()
 
+/******************************************************* INCLUDES ****************************************************/
 #include "catch.hpp"
 #include "stats.h"
 
 #include <math.h>
 
+/******************************************************* TEST CASES ***************************************************/
 TEST_CASE("reports average, minimum and maximum") {
     float numberset[] = {1.5, 8.9, 3.2, 4.5};
     int setlength = sizeof(numberset) / sizeof(numberset[0]);
     stats_s computedStats = {0,0,0};
     computedStats = compute_statistics(numberset, setlength);
     float epsilon = 0.001;
+    
     REQUIRE(abs(computedStats.average - 4.525) < epsilon);
     REQUIRE(abs(computedStats.max - 8.9) < epsilon);
     REQUIRE(abs(computedStats.min - 1.5) < epsilon);
@@ -52,8 +56,6 @@ TEST_CASE("raises alerts when max is greater than threshold") {
     const float maxThreshold = 10.2;
     check_and_alert(maxThreshold, alerters, computedStats);
 
-    // need a way to check if both emailAlerter, ledAlerter were called
-    // you can define call-counters along with the functions, as shown below
     REQUIRE(emailAlertCallCount == 1);
     REQUIRE(ledAlertCallCount == 1);
 }
