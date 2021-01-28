a4550c4d2a4001fd952e18fce8432b1f4e6908eb
VishalSubban <VishalSubban@users.noreply.github.com>
2021-01-28 20:14:04 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -10,7 +10,7 @@ typedef struct Statistics{
 stats_s compute_statistics(float* numberset, int setlength);
 
 typedef void (*alerter_funcptr)();
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], stats_s computedStats);
 
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
