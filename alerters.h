b79030454ad32ab5bc728509e1e99ac3e548f615
VishalSubban <VishalSubban@users.noreply.github.com>
2021-01-28 20:56:54 +0530

    Update alerters.h

--- alerters.h
+++ alerters.h
@@ -1,2 +1,14 @@
+
+/* Func Name: emailAlerter
+   Description: Raises an email alert based on conditions
+   Parameters: None
+   Return Type: void
+*/
 void emailAlerter();
+
+/* Func Name: ledAlerter
+   Description: Raises an LED alert based on conditions
+   Parameters: None
+   Return Type: void
+*/
 void ledAlerter();
